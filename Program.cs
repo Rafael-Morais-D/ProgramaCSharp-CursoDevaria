aa6f3d32e10b78a9bf7951efe7734cea6298266d
RafaelMorais <rafaelmorais.design@gmail.com>
2023-01-09 12:26:23 -0300

    Projeto inicial do curso de C# da Devaria

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿const string textoOlaMundo = "Olá mundo! Este é meu primeiro programa em C#, meu nome é";
+string nomeCompleto;
+int idade;
+
+/* idade = 31;
+nomeCompleto = "Rafael Morais";
+nomeCompleto = "Nome Mudado"; */
+
+Console.WriteLine("Qual o seu nome completo?");
+
+nomeCompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Quantos anos você tem?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+string fraseCompleta = $"{textoOlaMundo} {nomeCompleto} e eu tenho {idade} anos!";
+
+Console.WriteLine(fraseCompleta);
