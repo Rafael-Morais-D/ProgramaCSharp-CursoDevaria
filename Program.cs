4a82fc05891e4e240f4e84a82c6448f427a4f00f
RafaelMorais <rafaelmorais.design@gmail.com>
2023-01-10 13:22:27 -0300

    Fix

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿const string textoOlaMundo = "Olá mundo! Este é meu primeiro programa em C#, meu nome é";
+﻿/* DECLARAÇÂO DE VARIÁVEIS E CONSTANTES E OPERADOR AND COM IF E SWITCH CASE */
+
+/* const string textoOlaMundo = "Olá mundo! Este é meu primeiro programa em C#, meu nome é";
 string nomeCompleto;
 int idade;
-
-/* idade = 31;
-nomeCompleto = "Rafael Morais";
-nomeCompleto = "Nome Mudado"; */
 
 Console.WriteLine("Qual o seu nome completo?");
 
@@ -16,4 +14,65 @@ idade = Convert.ToInt32(Console.ReadLine());
 
 string fraseCompleta = $"{textoOlaMundo} {nomeCompleto} e eu tenho {idade} anos!";
 
-Console.WriteLine(fraseCompleta);
+if (idade == 31 && nomeCompleto == "Rafael Morais")
+{
+    Console.WriteLine($"{fraseCompleta} Você é proprietário desse computador.");
+}
+else
+{
+    Console.WriteLine($"{fraseCompleta} Parabéns, mas você não é proprietário desse computador.");
+}
+
+switch (nomeCompleto)
+{
+    case "Rafael Morais": Console.WriteLine($"{fraseCompleta} Você é proprietário desse computador."); break;
+    default: Console.WriteLine($"{fraseCompleta} Parabéns, mas você não é proprietário desse computador."); break;
+} */
+
+
+/* OPERADOR OR COM IF */
+
+/* string fastFood;
+
+Console.WriteLine("O que vamos jantar hoje?");
+
+fastFood = Console.ReadLine().ToString();
+
+if (fastFood == "Pizza" || fastFood == "Hamburger")
+{
+    Console.WriteLine($"Oba, adoro comer {fastFood}");
+}
+else
+{
+    Console.WriteLine($"Legal, mas preferia Pizza");
+} */
+
+
+/* VARIÁVEIS E OPERADOR UNÁRIO */
+
+/* idade = 31;
+nomeCompleto = "Rafael Morais";
+nomeCompleto = "Nome Mudado"; 
+
+bool valido = true;
+Console.WriteLine(!valido);
+Console.WriteLine(valido); */
+
+
+/* DESAFIO */
+
+string nomeJogador;
+
+Console.WriteLine("Qual o nome do jogador?");
+
+nomeJogador = Console.ReadLine().ToString();
+
+switch (nomeJogador)
+{
+    case "Rafael": case "Rodrigo": case "Renata": case "Miriam": case "Newton": case "Aida": case "Fabiana": case "Leandro":
+        Console.WriteLine("Jogador autorizado");
+        break;
+    default:
+        Console.WriteLine("Jogador não autorizado");
+        break;
+}
